1e2815adbd1f5672a0c332ba11db37ea350b3fc4
yamada-jikennya <92251718+yamada-jikennya@users.noreply.github.com>
2021-10-25 13:22:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-
-
 function Play(){
     alert("Play") //選択に合致するゲーム画面をstatic/display.xmlから呼び出す
 }
 function Setting(){
     $("body").html = "<p>ここにListを挿入</p>"
 }
+if(localStorage.getItem(['history'])===null){
+    locasStorage.setItem(['history'],[0]);
+}
+let static_src=[
+    "static/display.xml"
+];
+let request = new XMLHttpRequest;
+request.onreadystatechange=function(){
+    if(request.readyState===4&&request.status===200){
+        document.getElementById("main").innerHTML = request.response;
+    }
+}
+request.open('GET',static_src[localStorage.getItem(['history'])]);
+request.send(null);
