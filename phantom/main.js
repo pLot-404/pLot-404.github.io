ee870f6b6ae1db43619bd586258bcef1134a57ed
Geronimo-moon <Euler.ecosisin@gmail.com>
2022-10-30 21:41:28 +0000

    add phantom

--- phantom/main.js
+++ phantom/main.js
@@ -1,20 +1,18 @@
 let bgmls = {
-  music1: "bgm/first.mp3",
   music2: "bgm/second.mp3",
-  music3: "bgm/third.mp3",
   music4: "bgm/forth.mp3",
   music5: "bgm/fifth.mp3",
   music6: "bgm/sixth.mp3",
   music7: "bgm/second.mp3",
+  music8: "bgm/7buuu.wav",
   se1: "bgm/close.mp3",
-  se2: "bgm/tryopen.mp3",
-  se3: "bgm/mask.mp3",
-  se4: "bgm/manymask.mp3",
-  se5: "bgm/窓をノック1.mp3",
-  se6: "bgm/gh1_4__=4 (1).mp3",
   se7: "bgm/ガラスにひびが入る.mp3",
   se8: "bgm/ガラスが割れる1.mp3",
-  se9: "bgm/himei_kyaa_reverb.wav"
+  se9: "bgm/himei_kyaa_reverb.wav",
+  se10: "bgm/moshi1.wav",
+  se11: "bgm/nigasanai.wav",
+  se12: "bgm/yurusanai.wav",
+  se13: "bgm/phone.wav"
 };
 
 let bgm = new Audio();
@@ -51,7 +49,7 @@ $(function () {
       let winHeight = $(window).height();
       let scrollTop = $(window).scrollTop();
       let showClass = "show";
-      let timing = contentHeight*0; // 100pxコンテンツが見えたら次のif文がtrue
+      let timing = contentHeight * 0; // 100pxコンテンツが見えたら次のif文がtrue
       if (scrollTop >= contentsPOS - winHeight + timing) {
         $(elem).addClass(showClass);
       } else {
@@ -86,6 +84,9 @@ function scroll_effect() {
   //突然現れて操作不能にする
   $(".scroll-appear").each(function () {
     let yy = $(this).offset().top + $(this).height() / 3; //効果発生開始タイミングを操作したい場合は数値を変更する
+
+    let clsls = $(this).attr("class").split(" ");
+
     if (tt > yy - hh / 2) {
       $("html *").css("overflow", "hidden");
       $(window).on("touchmove.noScroll", function (e) {
@@ -97,6 +98,80 @@ function scroll_effect() {
       }, 2000);
       $(this).removeClass("scroll-appear");
       $(this).addClass("scroll-done");
+
+      if (clsls.indexOf("mute") != -1) {
+        bgm.pause();
+      }
+    }
+  });
+
+  $(".chakusin").each(function () {
+    let yy = $(this).offset().top; //効果発生開始タイミングを操作したい場合は数値を変更する
+    if (tt > yy - hh/2) {
+      $("html *").css("overflow", "hidden");
+      $(window).on("touchmove.noScroll", function (e) {
+        e.preventDefault();
+      });
+      $("#chakusin").css("display", "block");
+      setTimeout(function () {
+        $("#chakusin").css("display", "none");
+        $("html *").css("overflow", "visible");
+        $(window).off(".noScroll");
+      }, 3000);
+      $(this).removeClass("chakusin");
+      bgm.pause();
+      bgm.src = bgmls["se13"];
+      bgm.load();
+      bgm.play();
+      bgm.addEventListener("ended", function () {
+        bgm.src = bgmls[wbgm];
+        bgm.load();
+        bgm.play();
+      });
+      window.navigator.vibrate([900, 100, 900, 100, 900, 100]);
+    }
+  });
+
+  $(".break").each(function () {
+    let yy = $(this).offset().top; //効果発生開始タイミングを操作したい場合は数値を変更する
+    if (tt > yy - hh/2) {
+      $("#break").css("display", "block");
+      bgm.pause();
+      bgm.src = bgmls["se7"];
+      bgm.load();
+      bgm.play();
+      bgm.addEventListener("ended", function () {
+        bgm.src = bgmls[wbgm];
+        bgm.load();
+        bgm.play();
+      });
+      $(this).removeClass("break")
+    }
+  });
+
+  $(".broken").each(function () {
+    let yy = $(this).offset().top; //効果発生開始タイミングを操作したい場合は数値を変更する
+    if (tt > yy - hh/2) {
+      $("#break").css("display", "none");
+      bgm.pause();
+      bgm.src = bgmls["se8"];
+      bgm.load();
+      bgm.play();
+      bgm.addEventListener("ended", function () {
+        bgm.src = bgmls[wbgm];
+        bgm.load();
+        bgm.play();
+      });
+      $(this).removeClass("broken")
+    }
+  });
+
+  $(".reload").each(function () {
+    let yy = $(this).offset().top; //効果発生開始タイミングを操作したい場合は数値を変更する
+    if (tt > yy - hh/2) {
+      setTimeout(function(){
+        window.location.href = '/'
+      },5000)
     }
   });
 }
